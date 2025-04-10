fa89ebe3dbf27afa869bb3c7d831332d8d53d69c
anggadanarp <anggadanarp@gmail.com>
2025-04-10 12:22:46 +0800

    deploy: on Niskala network

--- config/CollectionConfig.ts
+++ config/CollectionConfig.ts
@@ -10,7 +10,7 @@ const CollectionConfig: CollectionConfigInterface = {
     platformAddressForLocalHost: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
     domainEip712: "DataSharing",
     versionDomain: "1",
-    contractAddress: "0x922872cA2B2FC36cE54EF998Ce1532D774A6511E",
+    contractAddress: "0x5740354Db8a705b9d6C35C487C137921776Fed7B",
     //marketplaceIdentifier: "market-place-identifier",
     //marketplaceConfig: Marketpalce.openSea
 };
